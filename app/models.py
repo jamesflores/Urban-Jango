f59c8636ecf42a572febf8b67b50d078dbbafe6f
James Flores <james@jamesflores.net>
2024-03-25 10:36:22 +1100

    add str function to SearchLog model

--- app/models.py
+++ app/models.py
@@ -6,3 +6,6 @@ class SearchLog(models.Model):
     ip_address = models.GenericIPAddressField()
     referrer = models.URLField(max_length=1024, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[{self.timestamp}] {self.query} from {self.ip_address} via {self.referrer}"
